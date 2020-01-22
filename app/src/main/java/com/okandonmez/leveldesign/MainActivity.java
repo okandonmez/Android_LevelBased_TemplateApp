18f1edc2e9c40db0a43967fb8c3c396d3b748886
okandonmez <ce.okandonmez@gmail.co>
2020-01-22 19:43:36 +0300

    Splash screen designed.

--- app/src/main/java/com/okandonmez/leveldesign/MainActivity.java
+++ app/src/main/java/com/okandonmez/leveldesign/MainActivity.java
@@ -3,14 +3,25 @@ package com.okandonmez.leveldesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
